fe754fabaec3d428021ccfc9f97cae741cf2294b
zihun <muhammadrizki.personal@gmail.com>
2024-10-28 13:42:21 +0700

    upload tugas UTS

---
+++ pem_web/UTS/index.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', function(){
+    alert('Berhasil ke laman Selanjutnya');
+})
+const img = document.getElementById('img')
+img.addEventListener('click', function(){
+    alert('Hello ini di bawah gambar adalah sosial Media kami, silahkan klik untuk mengeksplor lebih lanjut ya!');
+})
