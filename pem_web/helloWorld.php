b28da7efeae39525d2f32d234f53ecab674bd926
iniuyeng <aurelseptia7@gmail.com>
2024-12-16 11:03:08 +0700

    Add files via upload

---
+++ pem_web/helloWorld.php
@@ -0,0 +1,5 @@
+<?php
+//ini adalah first program php saya 
+echo "<h1>Hello World</h1>";
+/*ini adalah paragraf pertama saya dengan php */
+echo "\n<p>ini adalah paragraf pertama saya dengan php</p>";
