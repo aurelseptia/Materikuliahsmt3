e253015261ca29732ab227482ce3bb4c7e1f1c01
iniuyeng <aurelseptia7@gmail.com>
2024-12-24 00:39:36 +0700

    Add files via upload

---
+++ PBO/PBO/hallog.py
@@ -0,0 +1,10 @@
+from kivymd.app import from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivy.lang import Builder
+
+class super(MDScreen):
+    def build(self, **kw):
+        return 
+
+if __name__=="__main__":
+    super().run()
